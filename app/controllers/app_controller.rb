a15339aff785c770cfbcfb6d48036a65756073df
Mohammed <atefhamosa@gmail.com>
2021-02-16 01:36:43 +0200

    fix home imgs

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -1,8 +1,8 @@
 class AppController < ApplicationController
+    before_action :authenticate_user!, except: %i[index]
     def index
     end
     def cats
         @categories= Category.all
     end
-    
 end
