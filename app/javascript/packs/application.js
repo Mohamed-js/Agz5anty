9299602f1be856ebc66bfd503552abd390a3761f
Mohammed <atefhamosa@gmail.com>
2021-03-13 20:59:43 +0200

    make audio work

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,3 +12,46 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
+
+
+
+
+
+
+
+
+let count = 0
+function refresh() {
+    let req = new XMLHttpRequest();
+    
+    req.onreadystatechange = function (params) {
+        if (this.readyState === 4 && this.status === 200) {
+
+            let meds = JSON.parse(this.responseText);
+            document.getElementById("meds").innerHTML = ""
+            meds.forEach(med => {
+                document.getElementById("meds").innerHTML += med.name + "<br>"
+            });
+            if (count !== meds.length) {
+                document.getElementById("myAudio").play()
+            }
+            count = meds.length
+            
+        }
+    }
+    req.open('GET', '/medications.json', true);
+
+    req.send();
+}
+
+
+
+    setInterval(() => {
+        if (document.getElementById("meds") != null){
+            refresh()
+        }
+    }, 1000);
+
+
+
+
--- config/application.rb
+++ config/application.rb
@@ -8,9 +8,10 @@ Bundler.require(*Rails.groups)
 
 module Agz5anty
   class Application < Rails::Application
+    
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    config.assets.precompile << "audios/*"
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
