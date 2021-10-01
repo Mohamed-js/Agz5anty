301a5d675b71bc9061ee607907ee785fefac38c8
Mohammed <atefhamosa@gmail.com>
2021-10-01 03:17:28 +0200

    upload

--- app/models/medication.rb
+++ app/models/medication.rb
@@ -5,11 +5,12 @@ class Medication < ApplicationRecord
 
   
   def self.import
-    file = File.join(Rails.root, 'app', 'assets', 'drugs.csv')
+    file = File.join(Rails.root, 'public', 'ups', 'drugs.csv')
     CSV.foreach(file, headers: true) do |row|
       @med = Medication.new row.to_hash
       @med.category_id = 16 if !@med.category_id || @med.category_id == '-'
       @med.save
+      @med.valid?
     end
   end
 end
