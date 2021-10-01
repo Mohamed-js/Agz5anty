18ab46ba036ffaf8ef9c884a10cce18ad7331044
Mohammed <atefhamosa@gmail.com>
2021-10-01 03:07:43 +0200

    Update medication.rb

--- app/models/medication.rb
+++ app/models/medication.rb
@@ -8,6 +8,7 @@ class Medication < ApplicationRecord
     file = File.join(Rails.root, 'app', 'assets', 'drugs.csv')
     CSV.foreach(file, headers: true) do |row|
       @med = Medication.new row.to_hash
+      @med.category_id = 16 if !@med.category_id || @med.category_id == '-'
       @med.save
     end
   end
