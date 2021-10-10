29e6bb2a1f86da81a07c8255347fc250e63c47e3
Mohammed <atefhamosa@gmail.com>
2021-10-10 05:00:52 +0200

    Update search_controller.rb

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -6,7 +6,7 @@ module Api
       def index
         if params[:q] && params[:q] != ''
           @keyword = params['q'].to_s
-          @meds = Medication.where("lower(name) ILIKE '%#{@keyword}%'").or(Medication.where("lower(description) ILIKE '%#{@keyword}%'"))
+          @meds = Medication.where("lower(name) ILIKE '%#{@keyword}%'").or(Medication.where("lower(ar_name) ILIKE '%#{@keyword}%'"))
           @cosms = CosmMed.where("lower(name) ILIKE '%#{@keyword}%'").or(CosmMed.where("lower(description) ILIKE '%#{@keyword}%'"))
           @results = @meds + @cosms
         end
