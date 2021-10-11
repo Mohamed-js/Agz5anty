ab4cba5a92fe06b4fbe0166d6a5266aabd839e8d
Mohammed <atefhamosa@gmail.com>
2021-10-11 03:15:41 +0200

    Change Cosmetics import

--- app/models/cosm_cat.rb
+++ app/models/cosm_cat.rb
@@ -1,4 +1,4 @@
 class CosmCat < ApplicationRecord
   include ImageUploader::Attachment(:image)
-  has_many :cosm_meds, class_name: 'CosmMed', foreign_key: 'cosm_cat_id'
+  has_many :cosm_meds
 end
--- app/models/cosm_med.rb
+++ app/models/cosm_med.rb
@@ -1,6 +1,6 @@
 class CosmMed < ApplicationRecord
   include ImageUploader::Attachment(:image)
-  belongs_to :cosm_cat, class_name: 'CosmCat', foreign_key: 'cosm_cat_id'
+  belongs_to :cosm_cat
 
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :category, presence: true
@@ -10,7 +10,19 @@ class CosmMed < ApplicationRecord
 
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
-      CosmMed.create! row.to_hash
+      r = row.to_hash
+      med = CosmMed.new name: r['name'], price: r['price'], img: r['img'], category: r['category'], sub_category: r['sub_category']
+      if med.category && med.sub_category
+        cat = CosmCat.find_by('lower(name) = ?', med.sub_category.downcase)
+        if cat
+          med.cosm_cat_id = cat.id
+        else
+          cat = CosmCat.create(name: med.sub_category, parent: med.category)
+          med.cosm_cat_id = cat.id
+        end
+      end
+      med.availability = "true"
+      med.save
     end
   end
 end
